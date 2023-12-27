6afe35be15861ed5094c21608d6b38b12779e505
Martin Spicuzza <spicuzza157@gmail.com>
2023-12-26 22:30:15 -0600

    adding validations for name and description

--- app/models/card_type_attribute.rb
+++ app/models/card_type_attribute.rb
@@ -1,7 +1,11 @@
 class CardTypeAttribute < ApplicationRecord
-    validates :type, presence: true
 
-    def validate_type
-        errors.add(:type, "Cannot be ") if !type
-    end
+  validates :type, presence: true
+  validates :name, unique: true, presence: true
+  validates :descrption, presence: true
+
+  def validate_type
+    errors.add(:type, 'Cannot add or create a CardTypeAttribute of Null Type') unless type
+  end
+  
 end
