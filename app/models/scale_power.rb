cb9f89fa4f3a1f41445c8159f22788c0c45c612b
Martin Spicuzza <spicuzza157@gmail.com>
2023-12-29 19:25:16 -0600

    adding validations to ScalePowers

--- app/models/scale_power.rb
+++ app/models/scale_power.rb
@@ -1,2 +1,7 @@
 class ScalePower < ApplicationRecord
+    belongs_to :player_class
+
+    validates :name, :unique => true
+    validates :description, presence: true
+    validates :cost, presence: true, :numericality => { :greater_than_or_equal_to => 0 }
 end
