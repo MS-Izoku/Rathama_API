c72a9aa2b713209beb5e02f8bc24602f30ee73bc
Martin Spicuzza <spicuzza157@gmail.com>
2024-01-03 10:50:10 -0600

    adding regioning

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,25 @@
 Rails.application.routes.draw do
-# region Tribes
-  get 'card_type_attributes/tribes', to: 'card_type_attributes#tribes'
-  get 'tribes', to: 'card_type_attributes#tribes'
+#region CardTypeAttributes (Tribes and SpellSchools)
 
-  get 'card_type_attributes/tribe_names', to: 'card_type_attributes#tribe_names'
-  get 'tribes/names', to: 'card_type_attributes#tribe_names'
+  # region Tribes
+    get 'card_type_attributes/tribes', to: 'card_type_attributes#tribes'
+    get 'tribes', to: 'card_type_attributes#tribes'
+
+    get 'card_type_attributes/tribe_names', to: 'card_type_attributes#tribe_names'
+    get 'tribes/names', to: 'card_type_attributes#tribe_names'
+  # endregion
+
+
+  # region Spell Schools
+    get 'card_type_attributes/spell_schools', to: 'card_type_attributes#spell_schools'
+    get 'card_type_attributes/spell_school_names', to: 'card_type_attributes#spell_school_names'
+
+    get 'spell_schools', to: 'card_type_attributes#spell_schools'
+    get 'spell_schools/names', to: 'card_attributes#spell_school_names'
+  # endregion
+
 # endregion
 
-# region Spell Schools
-  get 'card_type_attributes/spell_schools', to: 'card_type_attributes#spell_schools'
-  get 'card_type_attributes/spell_school_names', to: 'card_type_attributes#spell_school_names'
-
-  get 'spell_schools', to: 'card_type_attributes#spell_schools'
-  get 'spell_schools/names', to: 'card_attributes#spell_school_names'
-# endregion
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
