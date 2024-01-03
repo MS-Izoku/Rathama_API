768aa456c9be74c919bd59e13e7e50f5aa60d661
Martin Spicuzza <spicuzza157@gmail.com>
2024-01-03 13:58:39 -0600

    adding controller routes for CardTypeAttributes, both Tribes and SpellSchools

--- app/controllers/card_type_attributes_controller.rb
+++ app/controllers/card_type_attributes_controller.rb
@@ -1,19 +1,34 @@
 class CardTypeAttributesController < ApplicationController
 
 # region: Tribes
+
   # return a list of all Tribes
   def tribes
     render json: Tribe.all
   end
+  
 
   # return a list of all Tribe Names
   def tribe_names
     render json: Tribe.all.select(:name)
   end
+
+
+  def tribes_alphebetically
+    render json: Tribe.all.order(name: :asc).select(:name)
+  end
+
+
+  def tribes_reverse_alphabetically
+    render json: Tribe.all.order(name: :desc).select(:name)
+  end
+
 # endregion
 
 
+
 # region: Spell Schools
+
   # return a list of all Spell Schools
   def spell_schools
     render json: SpellSchool.all
@@ -23,6 +38,17 @@ class CardTypeAttributesController < ApplicationController
   def spell_school_names
     render json: SpellSchool.all.select(:name)
   end
+
+
+  def spell_schools_alphebetically
+    render json: SpellSchool.all.order(name: :asc).select(:name)
+  end
+
+
+  def spell_schools_reverse_alphabetically
+    render json: SpellSchool.all.order(name: :desc).select(:name)
+  end
+
 #endregion
 
 
