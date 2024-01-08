e03e56941d9572ed8c3f3d9899f63b23bb366e00
Martin Spicuzza <spicuzza157@gmail.com>
2024-01-08 16:27:32 -0600

    changing location of the deck_limits_per_card_rarity

--- app/models/deck_card.rb
+++ app/models/deck_card.rb
@@ -5,21 +5,10 @@ class DeckCard < ApplicationRecord
     validate :validate_deck_card_count
 
 private
-    def self.deck_limits_per_card_rarity
-        {
-            "Common": 2,
-            "Uncommon": 2,
-            "Rare": 2,
-            "Epic": 2,
-            "Legendary": 1,
-            "Artifact": 1
-        }
-    end
-
     def validate_deck_card_count
         if deck.present? && card.present?
             card_count = deck.deck_cards.where(card: card).count
-            max_card_count = DeckCard.deck_limits_per_card_rarity[card.rarity]
+            max_card_count = Card.deck_limits_per_card_rarity[card.rarity]
 
             if card_count > max_card_count
                 errors.add(:base, "Deck already has the maximum number of allowed #{card.name} Cards (#{max_card_count})")
