c3b88908f29f445cf2fa72080d6f66d2d972f641
Martin Spicuzza <spicuzza157@gmail.com>
2024-01-17 16:05:19 -0600

    adding user model with secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ApplicationRecord
     has_secure_password
-    has_many :decks, foreign_key: "owner_id"    
+    has_many :decks, foreign_key: "owner_id"
 
     has_many :cards
-
 
     # the general  "Collection" of a user is their owned cards
     def collection
