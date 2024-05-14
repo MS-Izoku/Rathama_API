23863fc951e5b76d18254691eec2eb5601ffcfca
Martin Spicuzza <spicuzza157@gmail.com>
2024-05-14 14:13:27 -0500

    fixing implementation of version_manager

--- app/models/concerns/version_manager.rb
+++ app/models/concerns/version_manager.rb
@@ -6,37 +6,54 @@
 module VersionManager
   extend ActiveSupport::Concern
 
-# region: Version Numbering
-
-    # Version Numbers are Formatted like so: MAJOR.MINOR.PATCH
-    # Major: version increments when there are incompatible API changes (ie: Releases)
-    # Minor: version increments when functionality is added in a backward-compatible manner (ie: Features)
-    # Patch: version increments when backward-compatible bug fixes are made (ie: Bug Fixes)
-
-  def create_patch_number
-    version = parse_version
-    version[:patch] += 1
-    stringify_version(version)
+  included do
+    puts "VersionManager included in #{name}"
+    # This block runs when the module is included in a model
   end
 
-  def create_minor_increment
-    version = parse_version
-    version[:minor] += 1
-    version[:patch] = 0
-    stringify_version(version)
+
+  class_methods do
+
+      # turn the hashed version into a string
+      def stringify_version(version_hash)
+        "#{version_hash[:major]}.#{version_hash[:minor]}.#{version_hash[:patch]}"
+      end
+
+    def latest
+      order(created_at: :desc).first
+    end
+
+    def create_patch_number
+      version = latest.parse_version
+      version[:patch] += 1
+      stringify_version(version)
+    end
+
+    def create_minor_increment
+      version = latest.parse_version
+      version[:minor] += 1
+      version[:patch] = 0
+      stringify_version(version)
+    end
+
+
+
+
+    def create_major_increment
+      version = latest.parse_version
+
+      puts version
+
+      version[:major] += 1
+      version[:minor] = 0
+      version[:patch] = 0
+
+      stringify_version(version)
+    end
+
   end
 
-  def create_major_increment
-    version = parse_version
 
-    version[:major] += 1
-    version[:minor] = 0
-    version[:patch] = 0
-
-    stringify_version(version)
-  end
-
-    # turn the input version into a hash
   def parse_version
     split_string = version_number.split('.').map(&:to_i)
 
@@ -47,9 +64,4 @@ module VersionManager
     }
   end
 
-    # turn the hashed version into a string
-  def stringify_version(version_hash)
-    "#{version_hash[:major]}.#{version_hash[:minor]}.#{version_hash[:patch]}"
-  end
-# endregion
 end
--- app/models/concerns/versions.rb
+++ app/models/concerns/versions.rb
@@ -4,13 +4,5 @@
 module LatestVersion
   extend ActiveSupport::Concern
 
-  included do
-    # This block runs when the module is included in a model
-  end
 
-  class_methods do
-    def latest
-      order(created_at: :desc).first
-    end
-  end
 end
