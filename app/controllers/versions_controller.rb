00f0a5902fbe853c0f017827a0cf496188316460
Martin Spicuzza <spicuzza157@gmail.com>
2024-05-14 15:04:59 -0500

    filling out methods

--- app/controllers/versions_controller.rb
+++ app/controllers/versions_controller.rb
@@ -1,34 +1,48 @@
 class VersionsController < ApplicationController
+# region: API Versioning
+  def create_api_version
+    @new_version = ApiVersion.create_version(api_version_params[:version_name])
+    render json: @new_version
+  end
 
-    def create_api_version
-    end
+  def create_api_feature
+    @new_feature = ApiVersion.create_feature
+    render json: @new_feature
+  end
 
-    def create_api_feature
-    end
-
-    def create_api_patch
-    end
-
-
-
-    def create_client_version
-    end
-
-    def create_client_feature
-    end
-
-    def create_client_patch
-    end
-
-private
-
-    def update_api_version_params
-        params.require(:api_version).permit(:version_name)
-    end
+  def create_api_patch
+    @new_feature = ApiVersion.create_patch
+    render json: @new_feature
+  end
+# endregion
 
 
-    def update_client_version_parms
-        params.require(:client_version).permit(:version_name)
-    end
+# region: Client Versioning
 
+  def create_client_version
+    @new_version = ClientVersion.create_version(client_version_params[:version_name])
+    render json: @new_version
+  end
+
+  def create_client_feature
+    @new_feature = ClientVersion.create_feature
+    render json: @new_feature
+  end
+
+  def create_client_patch
+    @new_feature = ClientVersion.create_patch
+    render json: @new_feature
+  end
+
+# endregion
+
+  private
+
+  def api_version_params
+    params.require(:api_version).permit(:version_name)
+  end
+
+  def client_version_params
+    params.require(:client_version).permit(:version_name)
+  end
 end
