db06426155bf5a2afaa9e2468a4e2d2dff90288c
Martin Spicuzza <spicuzza157@gmail.com>
2024-05-14 10:33:27 -0500

    creating strong params

--- app/controllers/versions_controller.rb
+++ app/controllers/versions_controller.rb
@@ -1,2 +1,35 @@
 class VersionsController < ApplicationController
+
+# region: API Versioning
+    
+    # update the current version of the API (prompting the Server to Update when it "gets" the latest API Version)
+    def update_api_version
+    end
+
+    # get the current version of the API serviced (to check if the server needs to update)
+    def get_api_version
+    end
+
+# endregion
+
+
+# region: Client Versioning
+    
+    # Update the latest version of the Client (prompting the user to Update from the App Store)
+    def update_client_version
+    end
+
+# endregion
+
+private
+
+    def update_api_version_params
+        params.require(:api_version).permit(:version_number, :version_name)
+    end
+
+
+    def update_client_version_parms
+        params.requre(:client_version).permit(:version_number, :version_name)
+    end
+
 end
