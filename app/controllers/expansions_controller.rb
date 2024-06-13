b9488921e0ec72f415008c50d8bc5248eef8b90b
Martin Spicuzza <spicuzza157@gmail.com>
2024-06-13 10:47:41 -0500

    adding serialization for expansions controller

--- app/controllers/expansions_controller.rb
+++ app/controllers/expansions_controller.rb
@@ -1,31 +1,32 @@
 class ExpansionsController < ApplicationController
+    
+  def index
+    @expansions = Expansion.all.to_a
 
-    def index
-        @expansions = Expansion.all.to_a
+    if Rails.cache.exist?('expansions')
+      @expansions = Rails.cache.read('expansions')
+    else
+      @expansions = Expansion.all.to_a
+      Rails.cache.write('expansions', @expansions, expires_in: 12.hours)
+    end
+    render json: ExpansionSerializer.many(@expansions)
+  end
 
-        if Rails.cache.exist?("expansions")
-            @expansions = Rails.cache.read("expansions")
-        else
-            @expansions = Expansion.all.to_a
-            Rails.cache.write("expansions", @expansions, expires_in: 12.hours)
-        end
-        render json: ExpansionSerializer.many(@expansions)
+
+  def show
+    @expansion = Expansion.find_by(id: params[:id])
+    return render json: { error: 'Expansion not found' }, status: :not_found unless @expansion
+
+    key = "expansion_#{@expansion.id}"
+
+    if Rails.cache.exist?(key)
+      @expansion = Rails.cache.read(key)
+    else
+      Rails.cache.write(key, @expansion, expires_in: 12.hours)
     end
 
-    
-    def show
-        @expansion = Expansion.find_by(id: params[:id])
-        return render json: { error: 'Expansion not found' }, status: :not_found unless @expansion
-      
-        key = "expansion_#{@expansion.id}"
-      
-        if Rails.cache.exist?(key)
-          @expansion = Rails.cache.read(key)
-        else
-          Rails.cache.write(key, @expansion, expires_in: 12.hours)
-        end
-      
-        render json: ExpansionSerializer.new(@expansion).serializable_hash
-      end
+    render json: ExpansionSerializer.one(@expansion)
+  end
+
 
 end
