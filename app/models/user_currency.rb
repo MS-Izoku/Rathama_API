77301225a0b5daacb1fe086af13ed19b70ddd47c
Martin Spicuzza <spicuzza157@gmail.com>
2024-06-25 16:21:07 -0500

    adding user_currency asscocaitions and validation

--- app/models/user_currency.rb
+++ app/models/user_currency.rb
@@ -1,2 +1,6 @@
 class UserCurrency < ApplicationRecord
+    belongs_to :user
+    belongs_to :currency
+
+    validates :user_id, uniqueness: { scope: :currency_id, message: 'has already been taken for this currency' }
 end
