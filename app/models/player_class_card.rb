d823c3eb9b4b466ca5b0b1dcaed6ba6b4fa36443
Martin Spicuzza <spicuzza157@gmail.com>
2026-09-21 22:01:12 -0500

    adding fix for duplicate player class card association

--- app/models/player_class_card.rb
+++ app/models/player_class_card.rb
@@ -3,4 +3,15 @@
 class PlayerClassCard < ApplicationRecord
   belongs_to :player_class
   belongs_to :card
+
+  validate :validate_uniqueness
+
+  private
+
+  def validate_uniqueness
+    matches = PlayerClassCard.where(player_class_id:, card_id:)
+    return if matches.empty? || matches.first.id == id
+
+    errors.add(:base, 'This card is already associated with this player class.')
+  end
 end
